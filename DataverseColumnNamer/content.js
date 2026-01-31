3dd73963df0e4889df4bee5cf826f4773e1c12c2
PhuocLe <vanphuoc@gmail.com>
2026-01-31 17:47:38 +0700

    .

--- DataverseColumnNamer/content.js
+++ DataverseColumnNamer/content.js
@@ -352,6 +352,7 @@
     async function init() {
         await loadSettings();
         setupEnvironmentWatcher();
+        setupResizeListener();
 
         chrome.storage.onChanged.addListener((changes, namespace) => {
             if (namespace === 'local' && changes.DataverseColumnNamer) {
@@ -487,6 +488,16 @@
             characterData: true,
             childList: true,
             subtree: true
+        });
+    }
+
+    function setupResizeListener() {
+        let timeout;
+        window.addEventListener('resize', () => {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                checkAndScanEnvironment();
+            }, 500);
         });
     }
 
